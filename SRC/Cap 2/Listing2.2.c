a6816b70bb7b409344e246f2cb1ee6b8c932d5a4
maramoons <maraparra48@gmail.com>
2025-08-25 01:34:57 +0000

    Editar capitulo 2 y 3

--- SRC/Cap 2/Listing2.2.c
+++ SRC/Cap 2/Listing2.2.c
@@ -1,77 +1,105 @@
-Listing 2.2
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-/* The name of this program. */
+
+/* El nombre de este programa. */
 const char* program_name;
-/* Prints usage information for this program to STREAM (typically
- stdout or stderr), and exit the program with EXIT_CODE. Does not
- return. */
+
+/*
+ * Muestra información de uso para este programa en un flujo de datos (normalmente
+ * stdout o stderr) y termina el programa con un código de salida (exit_code).
+ * No regresa de la función.
+ */
 void print_usage (FILE* stream, int exit_code)
 {
- fprintf (stream, "Usage: %s options [ inputfile .... ]\n", program_name);
- fprintf (stream,
- 	" -h --help Display this usage information.\n"
- 	" -o --output filename Write output to file.\n"
- 	" -v --verbose Print verbose messages.\n");
- exit (exit_code);
+    fprintf (stream, "Uso: %s opciones [ archivo_de_entrada .... ]\n", program_name);
+    fprintf (stream,
+            " -h --help    Muestra esta información de uso.\n"
+            " -o --output  <nombre_de_archivo>  Escribe la salida en un archivo.\n"
+            " -v --verbose Muestra mensajes detallados.\n");
+    exit (exit_code);
 }
-/* Main program entry point. ARGC contains number of argument list
- elements; ARGV is an array of pointers to them. */
+
+/*
+ * Punto de entrada principal del programa.
+ * ARGC contiene el número de elementos de la lista de argumentos.
+ * ARGV es un array de punteros a esos elementos.
+ */
 int main (int argc, char* argv[])
 {
- int next_option;
- /* A string listing valid short options letters. */
-const char* const short_options = "ho:v";
- /* An array describing valid long options. */
- const struct option long_options[] = {
- { "help", 0, NULL, 'h' },
- { "output", 1, NULL, 'o' },
- { "verbose", 0, NULL, 'v' },
- { NULL, 0, NULL, 0 } /* Required at end of array. */
-};
-/* The name of the file to receive program output, or NULL for
- standard output. */
-const char* output_filename = NULL;
-/* Whether to display verbose messages. */
-int verbose = 0;
-/* Remember the name of the program, to incorporate in messages.
- The name is stored in argv[0]. */
-program_name = argv[0];
-do {
- 	next_option = getopt_long (argc, argv, short_options, long_options, NULL);
- 	switch (next_option)
- {
- 	case 'h': /* -h or --help */
- 		/* User has requested usage information. Print it to standard
- 		output, and exit with exit code zero (normal termination). */
- 		print_usage (stdout, 0);
- 	case 'o': /* -o or --output */
- 		/* This option takes an argument, the name of the output file. */
- 		output_filename = optarg;
- 		break;
- 	case 'v': /* -v or --verbose */
- 		verbose = 1;
- 		break;
- 	case '?': /* The user specified an invalid option. */
- 		/* Print usage information to standard error, and exit with exit
- 		code one (indicating abnormal termination). */
- 		print_usage (stderr, 1);
- 	case -1: /* Done with options. */
- 		break;
- 	default: /* Something else: unexpected. */
- 		abort ();
- 	}
+    int next_option;
+
+    /* Una cadena que lista las letras válidas de las opciones cortas. */
+    const char* const short_options = "ho:v";
+
+    /* Un array que describe las opciones largas válidas. */
+    const struct option long_options[] = {
+        { "help",    0, NULL, 'h' },
+        { "output",  1, NULL, 'o' },
+        { "verbose", 0, NULL, 'v' },
+        { NULL,      0, NULL, 0 }  /* Requerido al final del array. */
+    };
+
+    /* El nombre del archivo para la salida del programa, o NULL para la salida estándar. */
+    const char* output_filename = NULL;
+
+    /* Si se debe mostrar mensajes detallados. */
+    int verbose = 0;
+
+    /*
+     * Almacena el nombre del programa, para incluirlo en los mensajes.
+     * El nombre se encuentra en argv[0].
+     */
+    program_name = argv[0];
+
+    do {
+        next_option = getopt_long(argc, argv, short_options, long_options, NULL);
+
+        switch (next_option)
+        {
+            case 'h':   /* -h o --help */
+                /* El usuario ha solicitado información de uso. Imprímela en la salida
+                 * estándar y sal con el código de salida cero (terminación normal).
+                 */
+                print_usage (stdout, 0);
+                break; // Se agregó el 'break'
+
+            case 'o':   /* -o o --output */
+                /* Esta opción toma un argumento, el nombre del archivo de salida. */
+                output_filename = optarg;
+                break;
+
+            case 'v':   /* -v o --verbose */
+                verbose = 1;
+                break;
+
+            case '?':   /* El usuario especificó una opción inválida. */
+                /* Muestra información de uso en el error estándar y sal con el
+                 * código de salida uno (indicando terminación anormal).
+                 */
+                print_usage (stderr, 1);
+                break; // Se agregó el 'break'
+
+            case -1:    /* Terminado con las opciones. */
+                break;
+
+            default:    /* Algo más: inesperado. */
+                abort();
+        }
+    } while (next_option != -1);
+
+    /*
+     * Terminó el procesamiento de opciones. La variable 'optind' apunta al primer
+     * argumento que no es una opción. Para fines de demostración, los imprimimos
+     * si se especificó la opción 'verbose'.
+     */
+    if (verbose) {
+        int i;
+        for (i = optind; i < argc; ++i) {
+            printf ("Argumento: %s\n", argv[i]);
+        }
+    }
+    
+    /* El programa principal continuaría aquí. */
+    return 0;
 }
-while (next_option != -1);
-/* Done with options. OPTIND points to first nonoption argument.
- For demonstration purposes, print them if the verbose option was
- specified. */
- if (verbose) {
- 	int i;
- 	for (i = optind; i < argc; ++i)
- 		printf ("Argument: %s\n", argv[i]);
- }
- /* The main program goes here. */
- return 0;
-} 
